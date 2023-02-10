d35ee9224d07ea96e139345e91afba9243006e89
TensuAlsina <tensualsina57@gmail.com>
2023-02-10 14:03:20 +0300

    No commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";  
-import Navbar from "./components/navbar";
-import Home from './pages/Home/home';
-import Login from "./pages/Login/Login";
+ 
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/login' element={<Login/>}/>
-        </Routes>
-      </Router>
+    <div className="text-indigo-500">
+      Tensu
     </div>
   );
 }
